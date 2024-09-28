bdc7ede5746712cbcb59945cf8fc285f86c7420d
wackooswami <ztstexas@gmail.com>
2024-09-28 17:32:09 -0500

    merge conflict

--- src/routes/location_data/+page.svelte
+++ src/routes/location_data/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import type { exampleData } from "../../scripts/storage";
 
-    export let data: {places: exampleData[] };
+    export let data: exampleData;
 </script>
 
 {#await data}
     <p>Loading...</p>
-    {:then {places}}
-
+    {:then place}
         
-        <!-- {#each places as place}
-            <p>{place.Latitude}</p>
-            <p>{place.Longitude}</p>
-        {/each} -->
+        <p>Name: {place.Name}</p>
+        <p>latitude: {place.Latitude}</p>
+        <p>longitutde: {place.Longitude}</p>
+        <p>tags: {place.Tags}</p>
+        <p>stars: {place.Stars}</p>
+
+
 {/await}
