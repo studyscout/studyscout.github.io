65ce21f23192ef127a7295bc726ee24210915c2c
wackooswami <ztstexas@gmail.com>
2024-09-28 15:14:47 -0500

    Zach

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	export let bar = 'optional default initial value';
+	export let baz = undefined;
+</script>
