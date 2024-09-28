07e578d0910f2584de4e8484c805946be6732715
wackooswami <ztstexas@gmail.com>
2024-09-28 15:15:15 -0500

    zach

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
+<script lang="ts">
+  import places from "../scripts/storage";
+	let count = 0;
+	// Function to extract tag strings where value is true
+	const getTagList = (tags: Record<string, boolean>) => {
+		return Object.keys(tags).join(', ');
+	};
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<script>
-	export let bar = 'optional default initial value';
-	export let baz = undefined;
-</script>
+
+{#each places as place, index}
+  <a href="/location_data?id={index}"> go to {place.Name} <br></a>
+{/each}
