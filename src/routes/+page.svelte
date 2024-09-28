f99a7e476bda46a77addcb7f6ade2d78466afa15
Owen Shadburne <shadbowne@gmail.com>
2024-09-28 16:23:41 -0500

    Fix navigator error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,17 @@
 <script lang="ts">
-	let name = '';
-    let lat_long = '';
+	import type { Coordinates } from '../interfaces/interfaces';
+	import places from '../scripts/storage';
+	import useLocation from '../hooks/useLocation';
 
-    function retrieveLocation() {
-        if (navigator.geolocation) navigator.geolocation.getCurrentPosition(showLocation);
-        else lat_long = 'Not available';
-    }
+	useLocation(setLocation);
 
-    function showLocation(location : any) {
-        lat_long = 'Latitude: ' + location.coords.latitude + '\nLongitude: ' + location.coords.longitude;
-    }
+	function setLocation(coordinates: Coordinates) {
+		location = coordinates;
+	}
 
-    retrieveLocation();
-  import places from "../scripts/storage";
-	let count = 0;
-	// Function to extract tag strings where value is true
-	const getTagList = (tags: Record<string, boolean>) => {
-		return Object.keys(tags).join(', ');
-	};
-	import Counter from '../components/counter.svelte';
-	import Tags from '../components/tags.svelte';
+	let location: Coordinates | undefined;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Tags/>
 {#each places as place, index}
-  <a href="/location_data?id={index}"> go to {place.Name} <br></a>
+	<a href="/location_data?id={index}"> go to {place.Name} <br /></a>
 {/each}
-<Counter />
-<input bind:value={name} placeholder="enter your name" />
-<p>Hello {name || 'stranger'}!</p>
-<p>Location : {lat_long || 'unknown'}</p>
