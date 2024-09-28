704fd772f9d4deb7eb677528689642f4a5b452ce
Owen Shadburne <shadbowne@gmail.com>
2024-09-28 17:27:14 -0500

    Main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import type { Coordinates } from '../interfaces/interfaces';
 	import places from '../scripts/storage';
 	import useLocation from '../hooks/useLocation';
+	import LocationTile from '../components/locationTile.svelte';
+	import '../styles/global.sass';
+	import '../styles/home.sass';
 
 	useLocation(setLocation);
 
@@ -12,6 +15,9 @@
 	let location: Coordinates | undefined;
 </script>
 
-{#each places as place, index}
-	<a href="/locations?id={index}"> go to {place.Name} <br /></a>
-{/each}
+<div class="tile-list">
+	{#each places as place, index}
+		<LocationTile location={place} />
+		<!-- <a href="/locations?id={index}"> go to {place.Name} <br /></a> -->
+	{/each}
+</div>
