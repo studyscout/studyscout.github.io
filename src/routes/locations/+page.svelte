0db00141234535a9646e650397d8bc3db60ddb43
Owen Shadburne <shadbowne@gmail.com>
2024-09-28 16:31:54 -0500

    Name fixes

---
+++ src/routes/locations/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Location } from '../../scripts/storage';
+
+	export let data: Location;
+</script>
+
+{#await data}
+	<p>Loading...</p>
+{:then place}
+	<p>{place.Latitude}</p>
+	<p>{place.Longitude}</p>
+{/await}
--- src/routes/locations/+page.ts
+++ src/routes/locations/+page.ts
@@ -1,14 +1,14 @@
 import places from '../../scripts/storage';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ url}) => {
+export const load: PageLoad = async ({ url }) => {
 	/* const res = await fetch('https://jsonplaceholder.org/posts')
     const item = await res.json();
 
     return { posts: item}; */
-const a = url.searchParams;
-console.log(a.get("id"));
+	const a = url.searchParams;
+	console.log(a.get('id'));
 
 	const locaitons = places;
-	return { places: places};
+	return { places: places };
 };
