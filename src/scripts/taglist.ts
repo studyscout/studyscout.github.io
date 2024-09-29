b94488214dca1b5754cde2f57bbceef7e2bedfd2
wackooswami <ztstexas@gmail.com>
2024-09-29 01:17:02 -0500

    Zach: doing tag UI

---
+++ src/scripts/taglist.ts
@@ -0,0 +1,17 @@
+export const allTags: string[] = [
+	'Quiet',
+	'Hard to Find a Spot',
+	'Group Study Areas',
+	'Busy Times',
+	'Always a Seat Open',
+	'Frequent Outlet Access',
+	'Parking Nearby',
+	'Lots of Space',
+	'Natural Lighting'
+];
+
+export const accesbilities: string[] = [
+	'Wheel Chair Access',
+	'Deaf Friendly',
+	'Blind Friendly',
+];
